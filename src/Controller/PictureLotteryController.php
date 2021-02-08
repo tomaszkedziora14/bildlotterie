6be87f6d8bd614ae4fd63e5f8004d5c9ae115efd
tomaszkedziora14 <tomaszkedziora14@wp.pl>
2021-02-08 23:14:19 +0100

    fix

--- src/Controller/PictureLotteryController.php
+++ src/Controller/PictureLotteryController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\CrawlerInterface;
-use App\Service\FileUploader;
+use App\Service\FileUploaderInterface;
 
 class PictureLotteryController extends AbstractController
 {
@@ -15,7 +15,7 @@ class PictureLotteryController extends AbstractController
      */
     public function randomRemoteUploader(
         CrawlerInterface $imageCrawler,
-        FileUploader $fileUploder
+        FileUploaderInterface $fileUploder
     ): Response {
 
         $url = 'https://sklep.swiatkwiatow.pl/';
